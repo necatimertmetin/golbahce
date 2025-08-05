f0f09775c4df3e592d45f744e75de8ca6e8e537b
elif metin <elifxnur118@gmail.com>
2025-08-05 16:13:11 +0300

    source updated

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAaiNRnlPzXD9w5BmIaeFr72P70UTVh44s",
-  authDomain: "golbahce-54a5f.firebaseapp.com",
-  projectId: "golbahce-54a5f",
-  storageBucket: "golbahce-54a5f.firebasestorage.app",
-  messagingSenderId: "221634355798",
-  appId: "1:221634355798:web:f5f0965267b839ddfe51f7",
-  measurementId: "G-C5LW1XDYTZ",
+  apiKey: "AIzaSyCIeb_caJwulqQarjeMpAw7iUI0CpU_vu4",
+  authDomain: "golbahce-20148.firebaseapp.com",
+  projectId: "golbahce-20148",
+  storageBucket: "golbahce-20148.firebasestorage.app",
+  messagingSenderId: "162361008533",
+  appId: "1:162361008533:web:d0cf0a9d9fd58f8989d69c",
+  measurementId: "G-KG2NHDZFK3",
 };
 
 // Initialize Firebase
